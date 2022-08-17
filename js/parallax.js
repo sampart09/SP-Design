b3beafa53155fe862daff1051d5f3930e20a270b
sampart09 <sampart09@gmail.com>
2022-08-17 17:40:21 +0100

    1.3

--- js/parallax.js
+++ js/parallax.js
@@ -1,7 +1,5 @@
-let stars = document.getElementById('stars');
-let planet = document.getElementById('planet');
+let planet = document.getElementById('sun');
 let mountains = document.getElementById('mountains');
-let meteor = document.getElementById('meteor');
 let texts = document.getElementById('texts');
 let btn = document.getElementById('btn');
 let foreground = document.getElementById('foreground');
@@ -9,11 +7,8 @@ let header =document.querySelector('header');
 
 window.addEventListener('scroll', function () {
     let value = window.scrollY;
-    stars.style.left = value * 0.25 + 'px';
     planet.style.top = value * 1.05 + 'px';
     mountains.style.top = value * 0.25 + 'px';
-    meteor.style.marginTop = value * 2 + 'px';
-    meteor.style.marginLeft = value * 3 + 'px';
     foreground.style.top = value * 0 + 'px';
     text.style.marginRight = value * 3 + 'px';
     text.style.marginTop = value * 1.5 + 'px';
--- js/parallax2.js
+++ js/parallax2.js
@@ -1,13 +1,18 @@
+let sky = document.getElementById('sky');
 let caveback = document.getElementById('caveback');
 let moon = document.getElementById('cavemoon');
+let meteor = document.getElementById('meteor');
 let entrance = document.getElementById('entrance');
 let midground = document.getElementById('midground');
 let caveforeground = document.getElementById('caveforeground');
 
 window.addEventListener('scroll', function () {
     let value = window.scrollY;
-    caveback.style.top = value * 0 + 'px';
-    moon.style.top = value * 0.22 + 'px';
+    sky.style.left = value * 0.1 + 'px';
+    caveback.style.top = value * 0.04 + 'px';
+    moon.style.top = value * 0.3 + 'px';
+    meteor.style.marginTop = value * 0.07 + 'px';
+    meteor.style.marginLeft = value * 1.199 + 'px';
     entrance.style.top = value * 0 + 'px';
     midground.style.top = value * 0 + 'px';
     caveforeground.style.top = value * 0 + 'px';
