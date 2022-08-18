f9433f8e6c030d7fc8317f93177ef82b9af9473b
sampart09 <sampart09@gmail.com>
2022-08-18 09:56:36 +0100

    Update parallax2.js

--- js/parallax2.js
+++ js/parallax2.js
@@ -12,7 +12,7 @@ window.addEventListener('scroll', function () {
     caveback.style.top = value * 0.04 + 'px';
     moon.style.top = value * 0.3 + 'px';
     meteor.style.marginTop = value * 0.07 + 'px';
-    meteor.style.marginLeft = value * 1.199 + 'px';
+    meteor.style.marginLeft = value * 0.4 + 'px';
     entrance.style.top = value * 0 + 'px';
     midground.style.top = value * 0 + 'px';
     caveforeground.style.top = value * 0 + 'px';
